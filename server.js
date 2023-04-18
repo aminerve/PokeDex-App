0d2e2364e655c3011a9a3a60b465276b4df9c1fc
aminerve <122647915+aminerve@users.noreply.github.com>
2023-04-18 19:21:48 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,76 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = 3000
+// const pokemon = require('./models/pokemon');
+const React = require('react');
+const Pokemon = require('./models/PokemonSchema');
+const connectToDB = require('./config/db');
+
+//=============Configuring Engine
+app.set('view engine', 'jsx');
+app.engine('jsx', require('jsx-view-engine').createEngine());
+
+/*
+ * Middlewares
+ */
+app.use((req, res, next) => {
+    console.log('I run on all routes!');
+    next();
+
+});
+// Parsing incoming data from request
+// app.use(bodyparser.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// Home Route
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Index Route
+app.get('/pokemon', (req, res) => {
+    Pokemon.find({}, (error, allPokemon) => {
+      res.render('Index', {Pokemon: allPokemon});
+    })
+    // Pokemon.find({}, (error, allPoke) => {
+      // res.render('Index', {Pokemons: allPoke});
+    });
+// });
+
+/* 
+ * New Route
+ */
+app.get('/pokemon/new', (req, res) => {
+  res.render('New');
+});
+
+/**
+ * POST method
+ */
+app.post('/pokemon', (req, res) => {
+  console.log(req.body);
+  Pokemon.create(req.body).then(Pokemon => {
+    res.redirect('/pokemon')
+  }).catch((error) => {
+    console.error(error)
+  })
+})
+
+// Show Route
+app.get("/pokemon/:id", (req, res) => {
+    // Pokemons.findById(req.params.id, (error, foundPoke) => {
+    Pokemon.findById(req.params.id, (error, foundPokemon) => {
+      res.render('Show', {Pokemon: foundPokemon})
+    })
+    });
+  // });
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  connectToDB();
+});
---
+++ views/Index.jsx
@@ -0,0 +1,27 @@
+const React = require('react');
+const Pokemon = require('../models/PokemonSchema')
+
+
+function Index(props){
+    const { Pokemon } = props;
+    console.log(Pokemon)
+    return(
+        <>
+        <h1>Index of Pokemon</h1>
+        <nav>
+            <a href='/pokemon/new'>Catch a Pokemon!</a>
+        </nav>
+        <ul>
+            {Pokemon.map((poke) => {
+                return(
+                    <li key={poke._id}>
+                    <a href={`/pokemon/${poke._id}`}>{poke.name}</a></li>
+                )
+                
+            })}
+        </ul>
+        </>
+    )
+}
+
+module.exports = Index;
